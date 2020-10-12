be03e1e83174b5e2e2a84324c0ec6c3a75556adc
James Payne <jpayne@rrsc.com>
2020-10-12 10:07:27 -0400

    unified group work

--- Get-UnifiedGroup.ps1
+++ Get-UnifiedGroup.ps1
@@ -1,5 +1,9 @@
-Import-Module Connect-ExchangeOnline
+Import-Module ExchangeOnlineManagement
 $UserCredential = Get-Credential
 Connect-ExchangeOnline -Credential $UserCredential -ShowProgress $true
 
-Get-UnifiedGroup -Identity "Alert" | Format-List DisplayName, EmailAddresses, ManagedBy
+# Find the DistinguishedName of the person you want
+Get-Mailbox -Identity mpearmon | Format-List DistinguishedName
+
+# Thent his returns all groups where that person is the owner
+Get-UnifiedGroup -Filter { ManagedBy -eq 'CN=Mindy Pearmon,OU=rotorooter.onmicrosoft.com,OU=Microsoft Exchange Hosted Organizations,DC=NAMPR08A005,DC=PROD,DC=OUTLOOK,DC=COM' }
