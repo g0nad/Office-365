1ce15a8027b2099965e0ba2611ce3d8ba2019ead
James Payne <jpayne@rrsc.com>
2020-11-23 15:55:46 -0500

    added ad module

--- mainline_email_address_export_2.ps1
+++ mainline_email_address_export_2.ps1
@@ -1,1 +1,2 @@
-﻿Get-ADUser -Filter * -Properties DisplayName, EmailAddress, distinguishedname, Office | select DisplayName, EmailAddress, distinguishedname, Office | Export-CSV "C:\tmp\Email_Addresses.csv"
+﻿Import-Module activedirectory
+Get-ADUser -Filter * -Properties DisplayName, EmailAddress, distinguishedname, Office | select DisplayName, EmailAddress, distinguishedname, Office | Export-CSV "C:\tmp\Email_Addresses.csv"
